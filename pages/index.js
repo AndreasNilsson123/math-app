a4f340534b230482b498b4569b70e090da52246c
AndreasNilsson123 <Andreas.nisse.nilsson@outlook.com>
2021-05-06 22:32:19 +0200

    main

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,3 @@
-/* 
-import { Button } from "@material-ui/core";
-import Link from "next/link";
-
-
-export default function Home() {
-  return (
-    <>
-      <h1>Välj svårighetsgrad</h1>
-
-      <Link href="/easy">
-        <Button variant="contained" color="primary">Lätt</Button>
-      </Link>
-
-      <br/>
-
-      <Link href="/hard">
-        <Button variant="contained" color="secondary">Svår</Button>
-      </Link>
-    </>
-  );
-}
-*/
-
 import { Button } from "@material-ui/core";
 import Link from "next/link";
 import React from "react";
@@ -79,13 +55,3 @@ export default function Home() {
     </>
   );
 }
-
-/*
-      
-
-
-
-
-
-
-*/
